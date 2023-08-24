4c43b1591851d9fb7b70b482158cba54ffd34361
Jameel Ahmed <45817812+jameel-fullstackdeveloper@users.noreply.github.com>
2023-08-24 20:09:27 +0500

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -73,7 +73,8 @@
                             <div class="media">
                                 <div class="media-body">
                                  <h5 class="text-truncate" style="font-size:15px;color:#495057;text-transform: uppercase;margin-bottom:1px;">{{$val->employee->emp_firstname}} {{$val->employee->emp_lastname}}</h5>
-                                    <span class="text-warning">{{$val->employee->department->dept_name}}</span>
+                                    <span class="text-warning">{{$val->employee->department->dept_name}}</span><br>
+                                    <span style="font-size:15px">{{$val->employee->emp_phone}}</span>
                                     <p><a href="http://erp.sonafeeds.live/allemployees" style="color:#495057"> View Log <i class="fa fa-arrow-circle-right"></i></a></p>
                                 </div>
 
