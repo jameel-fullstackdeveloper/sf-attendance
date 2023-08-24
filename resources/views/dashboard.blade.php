d832f800c923545ed867d35d9a00609f37254bcb
Jameel Ahmed <45817812+jameel-fullstackdeveloper@users.noreply.github.com>
2023-08-24 19:37:23 +0500

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -78,7 +78,7 @@
 
 
                                     <h5 class="mb-2 text-success">  67 </h5>
-                                    <p><a href="http://erp.sonafeeds.live/allemployees" style="color:#495057"> View All <i class="fa fa-arrow-circle-right"></i></a></p>
+                                    <p><a href="http://erp.sonafeeds.live/allemployees" style="color:#495057"> View Log <i class="fa fa-arrow-circle-right"></i></a></p>
 
                                 </div>
 
@@ -100,55 +100,7 @@
 
 
 
-                                    <div class="card">
-                                        <div class="row no-gutters align-items-center">
-                                            <div class="col-md-4">
-                                            <a href="{{url('employee-attendace').'/'.$val->employee_id}}">
-                                                @if($val->employee->emp_photo!=null)
-                                                <img class="card-img"  src="data:image/png;base64,{{ base64_encode($val->employee->emp_photo) }}"  alt="No image">
-                                                @else
-                                                <img class="card-img" src="{{asset('assets/images/users/avatar.png')}}"  alt="No image">
-                                                @endif
-                                            </a>
-                                            </div>
 
-                                            <div class="col-md-8">
-                                                <div class="card-body">
-                                                <a href="{{url('employee-attendace').'/'.$val->employee_id}}">
-                                                    <h5>{{$val->employee->emp_firstname}} {{$val->employee->emp_lastname}}</h5>
-                                                </a>
-
-                                                    <p class="card-text">
-                                                        <div class="row">
-                                                            <div class="col-6">
-                                                                <span style="font-size:15px">Emp ID</span>
-                                                                <br>
-                                                                <span style="font-size:15px">Hired Date</span>
-                                                                <br>
-                                                                <span style="font-size:15px">Department</span>
-                                                                <br>
-                                                                <span style="font-size:15px">Cell #</span>
-                                                            </div>
-                                                            <div class="col-6">
-                                                                <span style="font-size:15px">{{$val->employee_id}}</span>
-                                                                <br>
-                                                                <span style="font-size:15px">{{ date('d-M-Y', strtotime($val->employee->emp_hiredate)) }}</span>
-                                                                <br>
-                                                                <span style="font-size:15px">{{$val->employee->department->dept_name}}</span>
-                                                                <br>
-                                                                <span style="font-size:15px">{{$val->employee->emp_phone}}</span>
-                                                                <br>
-                                                            </div>
-                                                        </div>
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <span class="custom-span waves-effect waves-light" @if($val->workstate==1) style="color:red" @endif>
-                                            <i class="bx bx-time-five"></i>&nbsp @if($val->workstate==0) Check-In @else Check-Out @endif &nbsp {{ date('h:i:s A', strtotime($val->punch_time)) }}
-
-                                        </span>
-                                    </div>
                             </div>
 
                             @endforeach
