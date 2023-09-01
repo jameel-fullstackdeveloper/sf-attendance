1b1bce4135b3ae17c2ba39270deb4752e54e10f1
Jameel Ahmed <45817812+jameel-fullstackdeveloper@users.noreply.github.com>
2023-09-01 14:40:32 +0500

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -69,8 +69,8 @@
                                         </div>
                                     </div>
                                     <hr/>
-                                        <span class="font-size-14" <?php if($val->workstate==1) { echo 'style="color:red"'; } else { echo  'style="color:green"';} ?> >
-                                                <i class="bx bx-time-five"></i>&nbsp @if($val->workstate==0) CheckIn @else CheckOut @endif &nbsp {{ date('h:i:s A', strtotime($val->punch_time)) }}
+                                        <span class="font-size-14" <?php if($val->workstate==1) { echo 'style="color:green"'; } else { echo  'style="color:red"';} ?> >
+                                                <i class="bx bx-time-five"></i>&nbsp @if($val->workstate==0) CheckOut @else CheckIn @endif &nbsp {{ date('h:i:s A', strtotime($val->punch_time)) }}
                                         </span>
                                 </div>
                             </div>
@@ -89,7 +89,7 @@
     var postData = {
         slug: slug
     };
-    
+
     var url = "{{ route('employees.show', ':slug') }}";
     url = url.replace(':slug', slug);
 
